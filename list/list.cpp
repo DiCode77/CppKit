e46c4e8fa76e47b5f1657f00f83f609f9d11995f
DiCode77 <dimazamshaq@icloud.com>
2025-11-24 00:06:58 +0200

    Added build settings.

---
+++ list/list.cpp
@@ -0,0 +1,8 @@
+//
+//  list.cpp
+//  CppKit
+//
+//  Created by DiCode77.
+//
+
+#include "list.hpp"
---
+++ list/list.hpp
@@ -0,0 +1,11 @@
+//
+//  list.hpp
+//  CppKit
+//
+//  Created by DiCode77.
+//
+
+#ifndef list_hpp
+#define list_hpp
+
+#endif /* list_hpp */
