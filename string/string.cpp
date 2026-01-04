55164737ad2d11bb82ba13b80b2e73f1677ae760
DiCode77 <dimazamshaq@icloud.com>
2026-01-04 16:35:16 +0200

    Code refactoring performed.

--- string/string.cpp
+++ string/string.cpp
@@ -1,1 +1,75 @@
 #include "string.hpp"
+
+dde::string::string(){
+    this->arr    = nullptr;
+    this->u_size = 0;
+}
+
+dde::string::string(dde::c_char_p_t str) : dde::string::string(){
+    if (str == nullptr){
+        return;
+    }
+    else{
+        this->u_size = GetStrlen(str);
+        this->arr = new char[this->u_size +1];
+        this->CopyStrToArr(str, arr, this->u_size +1);
+    }
+}
+
+dde::string::string(const dde::string &in) : dde::string::string(){
+    this->u_size = in.u_size;
+    this->arr = new char[this->u_size +1];
+    this->CopyStrToArr(in.arr, this->arr, this->u_size +1);
+}
+
+
+dde::string::string(dde::string &&in) : dde::string::string(){
+    this->arr = in.arr;
+    this->u_size = in.u_size;
+    
+    in.arr = nullptr;
+    in.u_size = 0;
+}
+
+dde::string::~string(){
+    delete [] this->arr;
+}
+
+dde::c_char_p_t dde::string::c_str(){
+    return this->arr;
+}
+
+dde::ulong_t dde::string::size() const{
+    return this->u_size;
+}
+
+dde::char_ref_t dde::string::at(ulong_t pos) const{
+    return *(this->arr + pos);
+}
+
+std::ostream& dde::operator<< (std::ostream &out, const dde::string &str){
+    return out << str.arr;
+}
+
+dde::char_ref_t dde::string::operator[] (dde::ulong_t pos){
+    return *(this->arr + pos);
+}
+
+dde::ulong_t dde::string::GetStrlen(dde::c_char_p_t str){
+    dde::ulong_t size = 0;
+    while (str[size] != '\0'){
+        size++;
+    }
+    return size;
+}
+
+void dde::string::CopyStrToArr(dde::c_char_p_t str, dde::char_p_t arr, dde::ulong_t in_size){
+    if (str == nullptr || arr == nullptr){
+        return;
+    }
+    else{
+        for (dde::ulong_t i = 0; i < in_size; i++){
+            arr[i] = str[i];
+        }
+    }
+}
--- string/string.hpp
+++ string/string.hpp
@@ -10,84 +10,33 @@
 #include <ostream>
 
 namespace dde{
+
+using char_p_t   = char*;
+using c_char_p_t = const char*;
+using ulong_t    = unsigned long;
+using char_ref_t = char&;
   
 class string{
-    char          *arr;
-    unsigned long u_size;
+    char_p_t arr;
+    ulong_t  u_size;
 public:
-    string(){
-        this->arr    = nullptr;
-        this->u_size = 0;
-    }
+    string();
+    string(c_char_p_t);
+    string(const string&);
+    string(string&&);
+    ~string();
     
-    string(const char *str) : string(){
-        if (str == nullptr){
-            return;
-        }
-        else{
-            this->u_size = GetStrlen(str);
-            this->arr = new char[this->u_size +1];
-            this->CopyStrToArr(str, arr, this->u_size +1);
-        }
-    }
+    c_char_p_t c_str();
+    ulong_t    size() const;
+    char_ref_t at(ulong_t) const;
     
-    string(const string &in) : string(){
-        this->u_size = in.u_size;
-        this->arr = new char[this->u_size +1];
-        this->CopyStrToArr(in.arr, this->arr, this->u_size +1);
-    }
+    friend std::ostream& operator<< (std::ostream&, const string&);
     
-    string(string &&in) : string(){
-        this->arr = in.arr;
-        this->u_size = in.u_size;
-        
-        in.arr = nullptr;
-        in.u_size = 0;
-    }
-    
-    ~string(){
-        delete [] this->arr;
-    }
-    
-    const char *c_str(){
-        return this->arr;
-    }
-    
-    unsigned long size() const{
-        return this->u_size;
-    }
-    
-    char &at(unsigned long pos) const{
-        return *(this->arr + pos);
-    }
-    
-    friend std::ostream& operator<< (std::ostream &out, const string &str){
-        return out << str.arr;
-    }
-    
-    char &operator[] (unsigned long pos){
-        return *(this->arr + pos);
-    }
+    char_ref_t operator[] (ulong_t);
     
 private:
-    unsigned long GetStrlen(const char *str){
-        unsigned long size = 0;
-        while (str[size] != '\0'){
-            size++;
-        }
-        return size;
-    }
-    
-    void CopyStrToArr(const char *str, char *arr, unsigned long in_size){
-        if (str == nullptr || arr == nullptr){
-            return;
-        }
-        else{
-            for (unsigned long i = 0; i < in_size; i++){
-                arr[i] = str[i];
-            }
-        }
-    }
+    ulong_t GetStrlen(c_char_p_t str);
+    void CopyStrToArr(c_char_p_t str, char_p_t arr, ulong_t in_size);
 };
 
 };
