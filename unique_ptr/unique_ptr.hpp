e843ff6d2eb7196487a5fc7f3e0c873a5debb9a6
DiCode77 <dimazamshaq@icloud.com>
2026-02-18 19:47:13 +0200

    Added unique_ptr class with implementation.

--- unique_ptr/unique_ptr.hpp
+++ unique_ptr/unique_ptr.hpp
@@ -8,4 +8,21 @@
 #ifndef unique_ptr_hpp
 #define unique_ptr_hpp
 
+namespace dde{
+
+template<typename Te>
+class unique_ptr{
+    Te *p_data;
+public:
+    unique_ptr() : p_data(nullptr){}
+    unique_ptr(Te *te) : p_data(te){}
+    
+    ~unique_ptr(){
+        delete this->p_data;
+        this->p_data = nullptr;
+    }
+};
+
+};
+
 #endif /* unique_ptr_hpp */
