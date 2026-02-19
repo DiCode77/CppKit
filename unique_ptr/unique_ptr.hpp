dd531adfab392cfd6b27c37ac838c836afd1b5c1
DiCode77 <dimazamshaq@icloud.com>
2026-02-19 14:49:35 +0200

    Added methods to the unique_ptr class for better interaction.

--- unique_ptr/unique_ptr.hpp
+++ unique_ptr/unique_ptr.hpp
@@ -16,11 +16,47 @@ class unique_ptr{
 public:
     unique_ptr() : p_data(nullptr){}
     unique_ptr(Te *te) : p_data(te){}
+    unique_ptr(unique_ptr &&u_ptr) : p_data(u_ptr.p_data){
+        u_ptr.p_data = nullptr;
+    }
     
     ~unique_ptr(){
-        delete this->p_data;
-        this->p_data = nullptr;
+        if (this->p_data != nullptr){
+            delete this->p_data;
+            this->p_data = nullptr;
+        }
     }
+    
+    Te *get() const noexcept{
+        return this->p_data;
+    }
+    
+    Te *release() noexcept{
+        Te *buff = this->p_data;
+        this->p_data = nullptr;
+        
+        return buff;
+    }
+    
+    void reset(Te *data = nullptr) noexcept{
+        if (this->p_data == data){
+            return;
+        }
+        
+        if (this->p_data != nullptr){
+            delete this->p_data;
+        }
+        this->p_data = data;
+    }
+    
+    void swap(unique_ptr &u_ptr) noexcept{
+        Te *dt = this->p_data;
+        this->p_data = u_ptr.p_data;
+        u_ptr.p_data = dt;
+    }
+    
+    unique_ptr(const unique_ptr&) = delete;
+    unique_ptr &operator=(const unique_ptr&) = delete;
 };
 
 };
