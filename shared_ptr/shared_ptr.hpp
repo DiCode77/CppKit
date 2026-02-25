d3f4e8b88b400636803fd7cd24ee34a03c13edbd
DiCode77 <dimazamshaq@icloud.com>
2026-02-25 22:47:30 +0200

    Added shared_ptr class with implementation.

--- shared_ptr/shared_ptr.hpp
+++ shared_ptr/shared_ptr.hpp
@@ -8,4 +8,29 @@
 #ifndef shared_ptr_hpp
 #define shared_ptr_hpp
 
+namespace dde{
+
+template <typename Te>
+class shared_ptr{
+    using ulong_t = unsigned long;
+    
+    ulong_t *strong;
+    ulong_t *weak;
+    Te      *data;
+public:
+    shared_ptr() : strong(nullptr), weak(nullptr), data(nullptr){}
+    shared_ptr(Te *t) : strong(new ulong_t(1)), weak(new ulong_t(0)), data(t){}
+    ~shared_ptr(){
+        *this->strong -= (this->strong != nullptr) ? 1 : 0;
+        
+        if (!*this->strong){
+            delete this->strong;
+            delete this->weak;
+            delete this->data;
+        }
+    }
+};
+
+}
+
 #endif /* shared_ptr_hpp */
