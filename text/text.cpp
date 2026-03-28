15bbd3d3575f29274e1dd5d8a53ade8c05d4870c
DiCode77 <dimazamshaq@icloud.com>
2026-03-28 22:01:55 +0200

    Added transformation constructor.

--- text/text.cpp
+++ text/text.cpp
@@ -1,1 +1,40 @@
 #include "text.hpp"
+
+dde::text::text() : set(0, dde::text::STR_CAPACITY), stg(new storage(new char[dde::text::STR_CAPACITY +1], 0, dde::text::STR_CAPACITY, nullptr, nullptr)){}
+
+dde::text::ulong_t dde::text::GetStrLen(c_char_p_t str) const{
+    ulong_t size = 0;
+    if (str != nullptr){
+        while (str[size] != '\0') {
+            size++;
+        }
+    }
+    return size;
+}
+
+dde::text::ulong_t dde::text::IncreaseCapacity(const dde::text::ulong_t &m_cap, const dde::text::ulong_t &size){
+    ulong_t capacity(m_cap);
+    
+    if (capacity < size){
+        while (capacity < size) {
+            capacity *= dde::text::STR_INCREASE;
+        }
+    }
+    
+    return capacity;
+}
+
+dde::text::char_p_t dde::text::GetNewArr(const dde::text::ulong_t &size){
+    return new char[size]{};
+}
+
+void dde::text::CopyStrToArr(dde::text::c_char_p_t str, dde::text::char_p_t arr, const dde::text::ulong_t &size){
+    if (!str || !arr || !size){
+        return;
+    }
+    else{
+        for (dde::text::ulong_t i = 0; i < size; i++){
+            arr[i] = str[i];
+        }
+    }
+}
--- text/text.hpp
+++ text/text.hpp
@@ -8,5 +8,62 @@
 #ifndef text_hpp
 #define text_hpp
 
+namespace dde{
+class text{
+    using ulong_t    = unsigned long;
+    using c_char_p_t = const char*;
+    using char_p_t   = char*;
+    using char_pp_t  = char**;
+    
+    static constexpr ulong_t     STR_CAPACITY = 35;
+    static constexpr ulong_t     STR_INCREASE = 2;
+    static constexpr const char *STR_VERSION  = "0.0.1";
+    static constexpr ulong_t     npos         = ~0;
+    
+    struct{
+        ulong_t size;
+        ulong_t capacity;
+    } set;
+    
+    struct storage{
+        char_p_t  arr;
+        ulong_t   size;
+        ulong_t   reserve;
+        storage   *back;
+        storage   *next;
+    } *stg;
+    
+public:
+    text();
+    text(c_char_p_t str) : text(){
+        if (str == nullptr){
+            return;
+        }
+        else{
+            ulong_t siz = this->GetStrLen(str);
+            ulong_t cap = this->IncreaseCapacity(STR_CAPACITY, siz);
+            
+            if (cap > STR_CAPACITY){
+                delete [] this->stg->arr;
+                this->stg->arr = this->GetNewArr(cap +1);
+                this->stg->size    = siz;
+                this->stg->reserve = cap;
+            }
+            
+            this->CopyStrToArr(str, this->stg->arr, siz +1);
+            this->set.size += siz;
+            this->set.capacity = cap;
+        }
+    }
+    
+private:
+    ulong_t GetStrLen(c_char_p_t) const;
+    ulong_t IncreaseCapacity(const ulong_t&, const ulong_t&);
+    char_p_t GetNewArr(const ulong_t&);
+    void CopyStrToArr(c_char_p_t, char_p_t, const ulong_t&);
+};
+
+
+};
 
 #endif /* text_hpp */
