a2da7218465df794d87aa40ccb8d6d845ac8d0cd
DiCode77 <dimazamshaq@icloud.com>
2026-04-09 23:18:28 +0300

    Fixed a bug that could prevent the destructor of standard types from being called.

--- vers/vers.hpp
+++ vers/vers.hpp
@@ -9,6 +9,7 @@
 #define vers_hpp
 #include <typeinfo>
 #include <cstdlib>
+#include <utility>
 
 namespace dde{
 
@@ -25,6 +26,7 @@ class vers{
     struct data{
         void *s;
         TYPE  t;
+        void (*dets)(void*) = nullptr;
     } t_info;
     
 public:
@@ -34,6 +36,11 @@ public:
     vers(Te t) : vers(){
         this->t_info.s = (Te*)std::malloc(sizeof(Te));
         new(this->t_info.s) Te(t);
+        
+        this->t_info.dets = [](void *p){
+            Te *cs = static_cast<Te*>(p);
+            cs->~Te();
+        };
     }
     
     ~vers(){
@@ -42,15 +49,20 @@ public:
     
     template <typename Te>
     vers &operator= (Te t){
-        if (this->t_info.s != nullptr){
-            this->Destroy<Te*>(this->t_info.s);
+        if (this->t_info.dets != nullptr){
+            this->Destroy(this->t_info.s);
         }
         
         this->t_info.s = (Te*)std::malloc(sizeof(Te));
-        new(this->t_info.s) Te(t);
+        new(this->t_info.s) Te(std::move(t));
+        
+        this->t_info.dets = [](void *p){
+            Te *cs = reinterpret_cast<Te*>(p);
+            cs->~Te();
+        };
+        
         return *this;
     }
-    
     
     template <typename Te>
     Te &get(){
@@ -58,14 +70,9 @@ public:
     }
     
 private:
-    
-    template <typename Te = void*>
-    void Destroy(void *des){
-        if (des != nullptr){
-            Te *t = (Te*)des;
-            t->~Te();
-            std::free(des);
-            
+    void Destroy(void *p){
+        if (this->t_info.dets != nullptr){
+            this->t_info.dets(p);
         }
     }
 };
