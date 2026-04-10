fdda88bb7e89e8afac9f720541e59cc16cbbe263
DiCode77 <dimazamshaq@icloud.com>
2026-04-10 23:27:55 +0300

    The code has been refactored and bugs have been fixed.

--- vers/vers.hpp
+++ vers/vers.hpp
@@ -14,51 +14,42 @@
 namespace dde{
 
 class vers{
-    enum class TYPE{
-        NONE,
-        BOOL,
-        CHAR,
-        INT,
-        LONG,
-        DOUBLE
-    };
+    using void_p_t  = void*;
+    using void_fp_t = void(*)(void*);
     
-    struct data{
-        void *s;
-        TYPE  t;
-        void (*dets)(void*) = nullptr;
-    } t_info;
+    struct Storage{
+        void_p_t  type;
+        void_fp_t dest;
+    } stg;
     
 public:
-    vers() : t_info(nullptr, TYPE::NONE){}
+    vers() : stg(nullptr, nullptr){}
     
     template <typename Te>
     vers(Te t) : vers(){
-        this->t_info.s = (Te*)std::malloc(sizeof(Te));
-        new(this->t_info.s) Te(t);
+        this->stg.type = static_cast<Te*>(std::malloc(sizeof(Te)));
+        new(this->stg.type) Te(t);
         
-        this->t_info.dets = [](void *p){
-            Te *cs = static_cast<Te*>(p);
-            cs->~Te();
+        this->stg.dest = [](void_p_t p_te){
+            static_cast<Te*>(p_te)->~Te();
         };
     }
     
     ~vers(){
-        this->Destroy(this->t_info.s);
+        this->Destroy(this->stg.type);
     }
     
     template <typename Te>
     vers &operator= (Te t){
-        if (this->t_info.dets != nullptr){
-            this->Destroy(this->t_info.s);
+        if (!this->empty()){
+            this->Destroy(this->stg.type);
         }
         
-        this->t_info.s = (Te*)std::malloc(sizeof(Te));
-        new(this->t_info.s) Te(std::move(t));
+        this->stg.type = static_cast<Te*>(std::malloc(sizeof(Te)));
+        new(this->stg.type) Te(t);
         
-        this->t_info.dets = [](void *p){
-            Te *cs = reinterpret_cast<Te*>(p);
-            cs->~Te();
+        this->stg.dest = [](void_p_t p_te){
+            static_cast<Te*>(p_te)->~Te();
         };
         
         return *this;
@@ -66,13 +57,20 @@ public:
     
     template <typename Te>
     Te &get(){
-        return *static_cast<Te*>(this->t_info.s);
+        return *static_cast<Te*>(this->stg.type);
+    }
+    
+    bool empty() const{
+        return !(this->stg.type != nullptr && this->stg.dest != nullptr);
     }
     
 private:
-    void Destroy(void *p){
-        if (this->t_info.dets != nullptr){
-            this->t_info.dets(p);
+    void Destroy(void_p_t p_te){
+        if (!this->empty()){
+            this->stg.dest(p_te);
+            this->stg.dest = nullptr;
+            std::free(this->stg.type);
+            this->stg.type = nullptr;
         }
     }
 };
