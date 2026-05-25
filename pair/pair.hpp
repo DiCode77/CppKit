31db2e2f9e52510766c9e9f6a4e2d6fdeeead0c0
DiCode77 <dimazamshaq@icloud.com>
2026-05-25 21:40:07 +0300

    Added the dde::pair class.

--- pair/pair.hpp
+++ pair/pair.hpp
@@ -8,4 +8,34 @@
 #ifndef pair_hpp
 #define pair_hpp
 
+namespace dde{
+
+template <typename Te_f, typename Te_s>
+class pair{
+    struct Storage{
+        Te_f first;
+        Te_s second;
+    } *stg;
+public:
+    pair() : stg(new Storage(Te_f{}, Te_s{})){}
+    pair(Te_f first, Te_s second) : pair(){
+        this->stg->first  = first;
+        this->stg->second = second;
+    }
+    
+    ~pair(){
+        delete this->stg;
+    }
+    
+    Te_f &first(){
+        return this->stg->first;
+    }
+    
+    Te_s &second(){
+        return this->stg->second;
+    }
+};
+
+}
+
 #endif /* pair_hpp */
